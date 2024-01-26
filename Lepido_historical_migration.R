3f62056fce9b90eec05249c3a6e638a3fd424635
baptiste-bovay <153264520+baptiste-bovay@users.noreply.github.com>
2024-01-26 08:43:10 +0100

    Update Lepido_historical_migration.R
    Add the section with the pakages

--- Lepido_historical_migration.R
+++ Lepido_historical_migration.R
@@ -1,13 +1,23 @@
-# # # # # # # # # # # # # # # # # # # # # # # # 
-# Historical migration of species in elevation
-# # # # # # # # # # # # # # # # # # # # # # # #
 
+# Code Shifting interactions under climate change
+#################################################
+
+# Code last update: 2024-01-26
+# R version 4.3.2 (2023-10-31 ucrt)
+# RStudio 2023.12.0+369 "Ocean Storm" Release (33206f75bd14d07d84753f965eaa24756eda97b7, 2023-12-17) for windows
+# Details of the R session at the end of the script
+
+# Authors:
 # Baptiste Bovay
-# nov. 2023
+# Patrice Descombes
+# Gaétan Glauser
+# Yannick Chittaro
+# Hanna Nomoto
+# Sergio Rasmann
 
 
-# 0. Set the script + packages
-##############################
+# 0. Packages, theme and functions 
+##################################
 
 # Clean R memory
 rm(list =ls()) 
@@ -246,6 +256,92 @@ par(mfrow = c(2, 2)) ; plot(MV_ZF[[5]]) ; par(mfrow = c(1, 1))
 
 sum(MV_MC[[2]]$n) # For M. celadussa
 sum(MV_ZF[[2]]$n) # For Z. filipendulae
+
+######
+
+
+# 4. Details of the R session
+#############################
+
+sessioninfo::session_info()
+
+
+#─ Session info ────────────────────────────────────────────────────────────────
+#setting  value
+#version  R version 4.3.2 (2023-10-31 ucrt)
+#os       Windows 10 x64 (build 19045)
+#system   x86_64, mingw32
+#ui       RStudio
+#language (EN)
+#collate  French_Switzerland.utf8
+#ctype    French_Switzerland.utf8
+#tz       Europe/Zurich
+#date     2024-01-26
+#rstudio  2023.12.0+369 Ocean Storm (desktop)
+#pandoc   NA
+
+#─ Packages ────────────────────────────────────────────────────────────────────
+#package     * version  date (UTC) lib source
+#abind         1.4-5    2016-07-21 [1] CRAN (R 4.3.0)
+#backports     1.4.1    2021-12-13 [1] CRAN (R 4.3.0)
+#boot          1.3-28.1 2022-11-22 [2] CRAN (R 4.3.2)
+#broom         1.0.5    2023-06-09 [1] CRAN (R 4.3.1)
+#car           3.1-2    2023-03-30 [1] CRAN (R 4.3.1)
+#carData       3.0-5    2022-01-06 [1] CRAN (R 4.3.1)
+#class         7.3-22   2023-05-03 [2] CRAN (R 4.3.2)
+#classInt      0.4-10   2023-09-05 [1] CRAN (R 4.3.2)
+#cli           3.6.1    2023-03-23 [1] CRAN (R 4.3.1)
+#codetools     0.2-19   2023-02-01 [2] CRAN (R 4.3.2)
+#colorspace    2.1-0    2023-01-23 [1] CRAN (R 4.3.1)
+#DBI           1.1.3    2022-06-18 [1] CRAN (R 4.3.1)
+#DHARMa      * 0.4.6    2022-09-08 [1] CRAN (R 4.3.1)
+#dplyr       * 1.1.2    2023-04-20 [1] CRAN (R 4.3.1)
+#e1071         1.7-13   2023-02-01 [1] CRAN (R 4.3.2)
+#fansi         1.0.4    2023-01-22 [1] CRAN (R 4.3.1)
+#generics      0.1.3    2022-07-05 [1] CRAN (R 4.3.1)
+#ggplot2     * 3.4.2    2023-04-03 [1] CRAN (R 4.3.1)
+#ggpubr      * 0.6.0    2023-02-10 [1] CRAN (R 4.3.1)
+#ggsignif      0.6.4    2022-10-13 [1] CRAN (R 4.3.1)
+#glue          1.6.2    2022-02-24 [1] CRAN (R 4.3.1)
+#gtable        0.3.3    2023-03-21 [1] CRAN (R 4.3.1)
+#KernSmooth    2.23-22  2023-07-10 [2] CRAN (R 4.3.2)
+#lattice       0.22-5   2023-10-24 [1] CRAN (R 4.3.2)
+#lifecycle     1.0.3    2022-10-07 [1] CRAN (R 4.3.1)
+#lme4          1.1-34   2023-07-04 [1] CRAN (R 4.3.1)
+#magrittr      2.0.3    2022-03-30 [1] CRAN (R 4.3.1)
+#MASS          7.3-60   2023-05-04 [2] CRAN (R 4.3.2)
+#Matrix        1.6-0    2023-07-08 [1] CRAN (R 4.3.1)
+#minqa         1.2.5    2022-10-19 [1] CRAN (R 4.3.1)
+#munsell       0.5.0    2018-06-12 [1] CRAN (R 4.3.1)
+#nlme          3.1-163  2023-08-09 [2] CRAN (R 4.3.2)
+#nloptr        2.0.3    2022-05-26 [1] CRAN (R 4.3.1)
+#pillar        1.9.0    2023-03-22 [1] CRAN (R 4.3.1)
+#pkgconfig     2.0.3    2019-09-22 [1] CRAN (R 4.3.1)
+#proxy         0.4-27   2022-06-09 [1] CRAN (R 4.3.2)
+#purrr         1.0.1    2023-01-10 [1] CRAN (R 4.3.1)
+#R6            2.5.1    2021-08-19 [1] CRAN (R 4.3.1)
+#raster      * 3.6-26   2023-10-14 [1] CRAN (R 4.3.2)
+#Rcpp          1.0.11   2023-07-06 [1] CRAN (R 4.3.1)
+#rlang         1.1.1    2023-04-28 [1] CRAN (R 4.3.1)
+#rstatix       0.7.2    2023-02-01 [1] CRAN (R 4.3.1)
+#rstudioapi    0.15.0   2023-07-07 [1] CRAN (R 4.3.1)
+#scales        1.2.1    2022-08-20 [1] CRAN (R 4.3.1)
+#sessioninfo   1.2.2    2021-12-06 [1] CRAN (R 4.3.2)
+#sf          * 1.0-14   2023-07-11 [1] CRAN (R 4.3.2)
+#sp          * 2.1-1    2023-10-16 [1] CRAN (R 4.3.2)
+#terra         1.7-55   2023-10-13 [1] CRAN (R 4.3.2)
+#tibble        3.2.1    2023-03-20 [1] CRAN (R 4.3.1)
+#tidyr         1.3.0    2023-01-24 [1] CRAN (R 4.3.1)
+#tidyselect    1.2.0    2022-10-10 [1] CRAN (R 4.3.1)
+#units         0.8-4    2023-09-13 [1] CRAN (R 4.3.2)
+#utf8          1.2.3    2023-01-31 [1] CRAN (R 4.3.1)
+#vctrs         0.6.3    2023-06-14 [1] CRAN (R 4.3.1)
+#withr         2.5.0    2022-03-03 [1] CRAN (R 4.3.1)
+
+#[1] C:/Users/Bovayb/AppData/Local/R/win-library/4.3
+#[2] C:/Program Files/R/R-4.3.2/library
+
+#───────────────────────────────────────────────────────────────────────────────
 
 ######
 
